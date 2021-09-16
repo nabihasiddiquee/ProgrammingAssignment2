ec0956b4a24e24427f66ebbb12945692ddb62108
nabihasiddiquee <90873685+nabihasiddiquee@users.noreply.github.com>
2021-09-17 03:55:43 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## to "caching the inverse of a matrix" rather than compute it repeatedly.
 
 ## In the present work, two functions are used to create a special object that 
-## stores a matrix and caches its inverse.
+## store a matrix and caches its inverse.
 
 ## makeCacheMatrix
 ## This function creates a special "matrix" object that can cache its inverse.
